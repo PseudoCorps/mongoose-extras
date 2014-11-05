636bc9ecfa3453b635584d6fd992fa69edfb44bf
hatim <hatimwahdani@hatims-MacBook-Pro.local>
2014-11-04 21:31:27 -0500

    initial commit

--- Schemas/ip.js
+++ Schemas/ip.js
@@ -1,10 +1,10 @@
 module.exports = function( mongoose ) {
-	var Types = mongoose.Schema.Types
-		,	util = require('util') 
-		, SchemaType = mongoose.SchemaType
-		, CastError = SchemaType.CastError
-		, regex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$|^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$|^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/
-		
+  var Types = mongoose.Schema.Types
+    , util = require('util') 
+    , SchemaType = mongoose.SchemaType
+    , CastError = SchemaType.CastError
+    , regex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$|^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$|^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/
+    
 
 /**
  * IP Schema Type constructor
@@ -15,16 +15,16 @@ module.exports = function( mongoose ) {
  * @api private
 */
 
-	function IP( key, options ) {
-		SchemaType.call( this, key, options, 'IP' );
-	};
+  function IP( key, options ) {
+    SchemaType.call( this, key, options, 'IP' );
+  };
 
 /*!
  * Inherits from SchemaType.
 */
 
-	IP.prototype = Object.create( SchemaType.prototype );
-	IP.prototype.constructor = IP;
+  IP.prototype = Object.create( SchemaType.prototype );
+  IP.prototype.constructor = IP;
 
 /**
  * Required validator for ip
@@ -47,25 +47,25 @@ module.exports = function( mongoose ) {
  * @api private
  */
 
-	IP.prototype.cast = function( value ) {
-		if ( value === null || value === '' ) {
-			return null;
-		}
-	
-		if ( !regex.test( value ) || 'undefined' === typeof value )	{
-			throw new CastError( 'ip', value, this.path );
-		}
+  IP.prototype.cast = function( value ) {
+    if ( value === null || value === '' ) {
+      return null;
+    }
+  
+    if ( !regex.test( value ) || 'undefined' === typeof value ) {
+      throw new CastError( 'ip', value, this.path );
+    }
 
-		if ( 'string' === typeof value ) {
-			return value;	
-		} 
-	
-		if ( value.toString ) {
-			return value.toString();
-		}
-		return value.toString();
+    if ( 'string' === typeof value ) {
+      return value; 
+    } 
+  
+    if ( value.toString ) {
+      return value.toString();
+    }
+    return value.toString();
 
-	};
+  };
 
 /*!
  * ignore
@@ -116,9 +116,9 @@ module.exports = function( mongoose ) {
       return this.cast( val );
     }
   };
-	
-	
-	
-	return mongoose.Schema.Types.IP = IP;
+  
+  
+  
+  return mongoose.Schema.Types.IP = IP;
 }
 
